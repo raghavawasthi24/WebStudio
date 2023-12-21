b359756e02a503d03be5ebc6b3a26e7b717c222d
Raghav Awasthi <raghavawasthi240@gmail.com>
2023-12-21 09:20:49 +0530

    added companies page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../../components/Header'
-import HeroSection from './components/HeroSection'
+import HeroSection from './sections/HeroSection'
+import Companies from './sections/Companies'
 
 export default function Home() {
   return (
     <div>
-      <Header/>
+      {/* <Header/> */}
       <HeroSection/>
+      <Companies/>
     </div>
   )
 }
---
+++ src/pages/Home/sections/Companies.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import zaiper from "../../../assests/zaiper.png";
+import spotify from "../../../assests/spotify.png";
+import zoom from "../../../assests/zoom.png";
+import slack from "../../../assests/slack.png";
+import amazon from "../../../assests/amazon.png";
+import adobe from "../../../assests/adobe.png";
+
+export default function Companies() {
+  const companies = [zaiper, spotify, zoom, slack, amazon, adobe];
+
+  return (
+    <section className="flex justify-center">
+      <div className="">
+        <h1 className="text-center my-4 text-gray-400">
+          Trusted By 250+ Companies
+        </h1>
+
+        <div className="flex px-2  lg:w-full flex-wrap items-center justify-center gap-3">
+          {companies.map((company, index) => (
+              <img src={company} alt="" className="w-[150px]" key={index}/>
+            
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
